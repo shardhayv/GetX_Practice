3e68f8bcdc9bda2a860c0103c8c3ce2245d1bd42
shardhayv <shardhayvatshyayan7@gmail.com>
2023-06-28 20:19:22 +0545

    GetX Dialog

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,10 +13,30 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: const Text('Home Page'),
       ),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          Card(
+            child: ListTile(
+              title: const Text('Dialog Alert'),
+              subtitle: const Text('GetX Dialog alert showing'),
+              onTap: () {
+                Get.defaultDialog(
+                    title: 'Confirm Okay',
+                    backgroundColor: Colors.red,
+                    middleText: 'Click Yes to Okay and No to No',
+                    textCancel: 'No',
+                    confirm: TextButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        child: const Text('Okay')));
+              },
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
